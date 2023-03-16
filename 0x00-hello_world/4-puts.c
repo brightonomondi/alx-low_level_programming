ae0e2f84d17eda065616df0196a68d1402575dc2
Brighton <brightonomondi4@gmail.com>
2023-03-16 13:47:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - this is the main functin
+ *
+ * Return: always 0
+ *\
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
