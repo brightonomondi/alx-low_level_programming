29ea3d3243342facfc46601d5d72b54b1910c999
Brighton <brightonomondi4@gmail.com>
2023-03-20 13:24:28 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio>
 
+/**
+ *
+ * main - last_digit.c
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	srand(time(0))
+
+		n = rand((n % 10(>)
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main - print_alphabets.c
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
+
