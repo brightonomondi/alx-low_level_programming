445787d71f5d2e956be519c08d77a30738f0c905
brightonomondi <brightonomondi4@gmail.com>
2023-03-24 14:12:12 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - check the code.
+*  *@x: The number to be checked
+* Return: 1 for uppercase letter or 0 for any else.
+*/
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
